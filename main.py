7d9ae7b248d6b0f5ee750d75d9680f87a9adf971
Vigo Walker <vigopaul05@gmail.com>
2024-01-01 17:50:05 -0300

    gvhhv

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = "Mi app con FastAPI"
 app.version = "0.0.1"
 
+movies = [
+    {
+        "id" : 1,
+        "title" : "movie 1",
+        "overview" : "la mejor pelicula del mundo",
+        "year" : "2009",
+        "rating" : 9.8,
+        "category" : "Accion"
+    }
+]
+
 @app.get('/', tags=['Home'])
 def message():
-    return "Hello World"
+    return HTMLResponse("<h1>Hello! World</h1> \n <p> So proud this works!!! </p>")
 
 @app.get('/test', tags=['Test'])
 def test():
     """This is for testing purposes"""
     return "this is a test"
+
+@app.get('/movies', tags=['Movies'])
+def get_movies():
+    return movies
