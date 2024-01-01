55bad041558075bb23cab59254b881b761b71241
Vigo Walker <vigopaul05@gmail.com>
2024-01-01 17:41:16 -0300

    gawdvgsgefd

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title = "Mi app con FastAPI"
+app.version = "0.0.1"
 
-@app.get('/')
+@app.get('/', tags=['Home'])
 def message():
     return "Hello World"
+
+@app.get('/test', tags=['Test'])
+def test():
+    """This is for testing purposes"""
+    return "this is a test"
